a12204dc0e4c91bc398e503685264d68309a56a6
chone <chonebz@gmail.com>
2017-03-15 18:06:22 +0800

    Add fastclick module.

--- proton-library/deps.js
+++ proton-library/deps.js
@@ -2,5 +2,6 @@
 // Please do not edit.
 goog.addDependency('../../../closure-templates/soyutils_usegoog.js', ['soy', 'soy.StringBuilder', 'soy.esc', 'soydata', 'soydata.SanitizedHtml', 'soydata.SanitizedHtmlAttribute', 'soydata.SanitizedJs', 'soydata.SanitizedJsStrChars', 'soydata.SanitizedUri', 'soydata.VERY_UNSAFE'], ['goog.asserts', 'goog.dom.DomHelper', 'goog.format', 'goog.i18n.BidiFormatter', 'goog.i18n.bidi', 'goog.soy', 'goog.soy.data.SanitizedContentKind', 'goog.string', 'goog.string.StringBuffer'], false);
 goog.addDependency('../../../proton-library/pt/ui/carousel.js', ['pt.ui.Carousel'], ['goog.Timer', 'goog.array', 'goog.dom.classlist', 'goog.ui.Component'], false);
+goog.addDependency('../../../proton-library/pt/ui/fastclick.js', ['pt.ui.FastClick'], [], false);
 goog.addDependency('../../../proton-library/pt/ui/selectable.js', ['pt.ui.Selectable'], ['goog.dom', 'goog.dom.classlist', 'goog.events', 'goog.events.EventType'], false);
 goog.addDependency('../../../proton-library/pt/ui/tabview.js', ['pt.ui.TabView'], ['goog.ui.Component'], false);
